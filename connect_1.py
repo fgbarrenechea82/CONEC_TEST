1c9f58ad8398323c6a8e9f95574f47760d143d8f
Facundo Gonzalo Ustariz <facundo.gonzalo.ustariz@gmail.com>
2024-03-10 10:39:42 -0300

    With Working Libraries

--- connect_1.py
+++ connect_1.py
@@ -1,9 +1,9 @@
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetMikoTimeoutException, NetMikoAuthenticationException
+from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException
 from paramiko import SSHException
 from getpass4 import getpass
 from datetime import datetime
-from os import path2 as os.path
+from os.path import exists
 import pyfiglet
 from colorama import Fore, Style
 
@@ -16,13 +16,13 @@ class ConnectAndPush:
       self.user=getpass('User Please: ')
       self.clave=getpass('Password Please: ')
 
-      if (exists('G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/DATA/DEVICE.yaml')): # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST DE HOST
-         self.ip_file='G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/DATA/DEVICE.yaml' # METELO EN LA VARIALBLE ip_file
+      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml')): # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST DE HOST
+         self.ip_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml' # METELO EN LA VARIALBLE ip_file
       else:
          print(Fore.RED + "La playlist de IP no esta disponible" + Style.RESET_ALL)
          self.archivos_conf = False
-      if (exists('G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/DATA/COMMANDS.txt')): #  # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST CON LOS COMANDOS
-         self.command_file='G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/DATA/COMMANDS.txt' # METELO EN LA VARIALBLE command_file 
+      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt')): #  # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST CON LOS COMANDOS
+         self.command_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt' # METELO EN LA VARIALBLE command_file 
       else:
          print(Fore.RED + "El archivo de Comandos no esta disponible" + Style.RESET_ALL)
          self.archivos_conf = False
@@ -48,7 +48,7 @@ class ConnectAndPush:
             try:
                hora = datetime.now().strftime("%H:%M:%S")   # ACTUALIZACION DE HORA
                ssh = ConnectHandler(**host)
-               resultado=open('G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/RESULT/'+fecha+'-resultados.txt', 'a')
+               resultado=open('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/RESULT/'+fecha+'-resultados.txt', 'a')
                resultado.write('\n- - - - - - - - - - -\n\n' + hora + ' --> ' + host['ip'])
                for coma in comandos:
                   salida = ssh.send_command(coma)
@@ -61,7 +61,7 @@ class ConnectAndPush:
 
             except (NetMikoTimeoutException, NetMikoAuthenticationException, SSHException) as e:
                print(e)
-               error=open('G:/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/RESULT/'+fecha+'-errores.txt', 'a')
+               error=open('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/RESULT/'+fecha+'-errores.txt', 'a')
                error.write('\n- - - - - - - - - - -\n' + hora + ' --> ' + str(e))   #INFORMA TIPO DE ERROR
                error.close()
                continue
