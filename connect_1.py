453ad4a5a43d7def9076f4d28b3d463a5b446572
Facundo Gonzalo Ustariz <facundo.gonzalo.ustariz@gmail.com>
2024-03-10 12:04:09 -0300

    Host dic 0.2

--- connect_1.py
+++ connect_1.py
@@ -1,11 +1,12 @@
-from netmiko import ConnectHandler
-from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException
-from paramiko import SSHException
-from getpass4 import getpass
-from datetime import datetime
-from os.path import exists
-import pyfiglet
-from colorama import Fore, Style
+from netmiko import ConnectHandler # CONNECTIONS
+from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException # CONNECTIONS
+from paramiko import SSHException # CONNECTIONS
+from getpass4 import getpass # MANAGE PASSWORD
+from datetime import datetime # DATE AND TIME INFORMATION
+from os.path import exists # TO CHECK FILES ON THE SYSTEM
+import pyfiglet # FORMAT TEXT
+from colorama import Fore, Style # TO SET COLOURS IN TERMINAL VIEW
+import yaml #  TO WORK WITH YALM FILES
 
 class ConnectAndPush:
    
@@ -13,16 +14,25 @@ class ConnectAndPush:
 
       self.ascii_banner = pyfiglet.figlet_format("SSH con Netmiko\n") # BANNER
       self.archivos_conf = True
-      self.user=getpass('User Please: ')
+      self.user=input('User Please: ')
       self.clave=getpass('Password Please: ')
 
-      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml')): # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST DE HOST
-         self.ip_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml' # METELO EN LA VARIALBLE ip_file
+      
+      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml')): # CHECK IF THE FILE EXISTS
+        self.ip_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/DEVICE.yaml' # POPULEATE THE VARIALBLE ip_file
       else:
          print(Fore.RED + "La playlist de IP no esta disponible" + Style.RESET_ALL)
          self.archivos_conf = False
-      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt')): #  # SI EXISTE UN ARCHIVO CON ESE NOMBRE EN LA RUTA DONDE ESTA EL PLAYLIST CON LOS COMANDOS
-         self.command_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt' # METELO EN LA VARIALBLE command_file 
+      
+      try:
+         with open(self.ip_file, 'r') as stream:
+            self.dispositivos = yaml.safe_load(stream)
+      except FileNotFoundError:
+            print(Fore.RED + "The DEVICE.yaml file is not found." + Style.RESET_ALL)
+            self.archivos_conf = False
+     
+      if (exists('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt')): # CHECK IF THE FILE EXISTS
+         self.command_file='/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/DATA/COMMANDS.txt' # POPULATE THE VARIALBLE command_file 
       else:
          print(Fore.RED + "El archivo de Comandos no esta disponible" + Style.RESET_ALL)
          self.archivos_conf = False
@@ -32,21 +42,19 @@ class ConnectAndPush:
          print(Fore.MAGENTA + self.ascii_banner + "\n" + Style.RESET_ALL)
          equipos=open(self.ip_file)
          fecha = datetime.now().strftime("%Y%m%d")
-
-         for IP in equipos:
-            comandos=open(self.command_file)  # VARIABLE QUE CARGA LOS COMANDOS A EJECUTAR
-            host = {
-               'device_type': 'cisco_ios', 
-               'ip': '10.110.255.25',
-               'port': '22',
-               'username': self.user,
-               'password': self.clave
-            }
-            print(Fore.LIGHTGREEN_EX + '\nConectandose a:' + host['ip'] + Style.RESET_ALL, end="")
+         
+         comandos=open(self.command_file)  # POPULATE THE VARIABLE comandos WITH THE EXECUTION COMMNADS
+         
+         for device_name, device_info in self.dispositivos.items():
+            host = device_info.copy()
+            #host['port'] = int(host['port'])
+            host['username']=self.user
+            host['password']=self.clave
+            print(Fore.LIGHTGREEN_EX + '\nConectandose a:' + device_name + ': ' + host['ip'] + Style.RESET_ALL)
             print('- - - - - - - - - - - -\n')
             
             try:
-               hora = datetime.now().strftime("%H:%M:%S")   # ACTUALIZACION DE HORA
+               hora = datetime.now().strftime("%H:%M:%S")   # CURRENT TIME
                ssh = ConnectHandler(**host)
                resultado=open('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/RESULT/'+fecha+'-resultados.txt', 'a')
                resultado.write('\n- - - - - - - - - - -\n\n' + hora + ' --> ' + host['ip'])
@@ -62,7 +70,7 @@ class ConnectAndPush:
             except (NetMikoTimeoutException, NetMikoAuthenticationException, SSHException) as e:
                print(e)
                error=open('/mnt/g/Otros ordenadores/Mi portátil/DEVNET/AUTOMATION/CONEC_TEST/RESULT/'+fecha+'-errores.txt', 'a')
-               error.write('\n- - - - - - - - - - -\n' + hora + ' --> ' + str(e))   #INFORMA TIPO DE ERROR
+               error.write('\n- - - - - - - - - - -\n' + hora + ' --> ' + str(e)) # TELL US THE ERROR
                error.close()
                continue
       else:
